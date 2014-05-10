ee5374c42e817b898d4b428094929de5f1072822
bootstraponline <code@bootstraponline.com>
2014-05-10 13:27:34 -0400

    Don't reorder ToC

--- source/javascripts/app/search.js
+++ source/javascripts/app/search.js
@@ -42,26 +42,7 @@
       .on('blur', inactive);
   }
 
-  function refToHeader (itemRef) {
-    return $('.tocify-item[data-unique=' + itemRef + ']').closest('.tocify-header');
-  }
-
-  function sortDescending (obj2, obj1) {
-    var s1 = parseInt(obj1.id.replace(/[^\d]/g, ''), 10);
-    var s2 = parseInt(obj2.id.replace(/[^\d]/g, ''), 10);
-    return s1 === s2 ? 0 : s1 < s2 ? -1 : 1;
-  }
-
-  function resetHeaderLocations () {
-    var headers = $(".tocify-header").sort(sortDescending);
-    $.each(headers, function (index, item) {
-      $(item).insertBefore($("#toc ul:first-child"));
-    });
-  }
-
   function search (event) {
-    var sections = $('section, #toc .tocify-header');
-
     searchInfo.hide();
     unhighlight();
 
@@ -69,40 +50,15 @@
     if (event.keyCode === 27) this.value = '';
 
     if (this.value) {
-      sections.hide();
-      // results are sorted by score in descending order
       var results = index.search(this.value);
-
       if (results.length) {
-        resetHeaderLocations();
-        var lastRef;
         $.each(results, function (index, item) {
-          if (item.score <= 0.0001) return; // remove low-score results
-          var itemRef = item.ref;
-          $('#section-' + itemRef).show();
-          // headers must be repositioned in the DOM
-          var closestHeader = refToHeader(itemRef);
-          if (lastRef) {
-            refToHeader(lastRef).insertBefore(closestHeader);
-          }
-          closestHeader.show();
-          lastRef = itemRef;
+          $('.tocify-item[data-unique=' + item.ref + ']').closest('.tocify-header').show();
         });
-
-        // position first element. it wasn't positioned above if len > 1
-        if (results.length > 1) {
-          var firstRef = results[0].ref;
-          var secondRef = results[1].ref
-          refToHeader(firstRef).insertBefore(refToHeader(secondRef));
-        }
-
         highlight.call(this);
       } else {
-        sections.show();
         searchInfo.text('No Results Found for "' + this.value + '"').show();
       }
-    } else {
-      sections.show();
     }
 
     // HACK trigger tocify height recalculation
